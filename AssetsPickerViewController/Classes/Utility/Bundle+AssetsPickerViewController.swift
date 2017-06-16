f4174c941898e6963913d74d58452803fabb5f95
DragonCherry <dragoncherry@naver.com>
2017-06-16 17:26:36 +0900

    .

--- AssetsPickerViewController/Classes/Utility/Bundle+AssetsPickerViewController.swift
+++ AssetsPickerViewController/Classes/Utility/Bundle+AssetsPickerViewController.swift
@@ -10,15 +10,26 @@ import Foundation
 import TinyLog
 
 extension Bundle {
-    static var assetsPickerBundle: Bundle {
-//        if let path = Bundle(for: AssetsPickerViewController.classForCoder()).path(forResource: "AssetsPickerViewController", ofType: "bundle") {
-//            if let bundle = Bundle(path: path) {
-//                log("\(bundle.resourcePath)")
-//                return bundle
-//            }
-//        }
-//        fatalError("Failed to find CocoaPods resource bundle.")
-        
+    
+    private static let kAssetsPickerResourceName = "AssetsPickerViewController"
+    private static let kAssetsPickerResourceType = "bundle"
+    
+    static let assetsRootBundle: Bundle = {
         return Bundle(for: AssetsPickerViewController.classForCoder())
+    }()
+    
+    static let assetsPickerPath: String? = {
+        return Bundle.assetsRootBundle.path(forResource: Bundle.kAssetsPickerResourceName, ofType: Bundle.kAssetsPickerResourceType)
+    }()
+    
+    static var assetsPickerBundle: Bundle {
+        if let path = assetsPickerPath {
+            if let bundle = Bundle(path: path) {
+                return bundle
+            } else {
+                logw("Failed to get localized bundle.")
+            }
+        }
+        return assetsRootBundle
     }
 }
